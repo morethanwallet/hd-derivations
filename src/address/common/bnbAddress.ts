38f09a44a00269203144588e04120706bfbae8e3
paul <kyrogane1@gmail.com>
2025-01-13 08:23:13 +0000

    refactor: Adapt bnb derivation to new logic

--- src/address/common/bnbAddress.ts
+++ src/address/common/bnbAddress.ts
@@ -1,33 +1,34 @@
-import { Keys } from "./keys/index.js";
 import { ecPair, type ECPairInterface } from "@/ecc/index.js";
 import {
   getPublicKeyFromPrivateKey,
   getAddressFromPrivateKey,
 } from "@binance-chain/javascript-sdk/lib/crypto";
-import { appendAddressToDerivationPath, removeDerivationPathAddress } from "../helpers/index.js";
 import { assert, toHexFromBytes, toUint8Array } from "@/helpers/index.js";
 import { ExceptionMessage, AddressError } from "../exceptions/index.js";
-import { EMPTY_MNEMONIC, SEARCH_FROM_MNEMONIC_LIMIT } from "../constants/index.js";
 import { type Mnemonic } from "@/mnemonic/index.js";
+import { Keys } from "@/keys/bip32/index.js";
 import {
-  type AddressList,
-  type AbstractAddress,
-  type DerivedItem,
-  type KeysConfig,
-  type KeyPair,
-} from "@/address/index.js";
+  AbstractKeyDerivation,
+  DerivedCredential,
+  DerivedItem,
+  DerivedKeyPair,
+  DeriveFromMnemonicParameters,
+  ImportByPrivateKeyParameters,
+} from "../types/index.js";
+import { DerivationType } from "../enums/derivationType.enum.js";
+import { KeysConfig } from "@/keys/types/index.js";
 
 const HRP = "bnb";
 
-class BnbAddress extends Keys implements AbstractAddress<typeof AddressList.BNB> {
+class BnbAddress extends Keys implements AbstractKeyDerivation<typeof DerivationType.BNB> {
   public constructor(keysConfig: KeysConfig, mnemonic: Mnemonic) {
     super(keysConfig, mnemonic);
   }
 
-  public derive({
+  public deriveFromMnemonic({
     derivationPath,
-  }: Parameters<AbstractAddress<typeof AddressList.BNB>["derive"]>[0]): DerivedItem<
-    typeof AddressList.BNB
+  }: DeriveFromMnemonicParameters<typeof DerivationType.BNB>): DerivedItem<
+    typeof DerivationType.BNB
   > {
     const node = this.rootKey.derivePath(derivationPath);
     const { privateKey, publicKey } = this.getKeyPair(node.privateKey);
@@ -37,29 +38,27 @@ class BnbAddress extends Keys implements AbstractAddress<typeof AddressList.BNB>
       privateKey,
       publicKey,
       address,
-      path: derivationPath,
-      mnemonic: this.mnemonic.getMnemonic(),
+      derivationPath,
     };
   }
 
   public importByPrivateKey({
-    derivationPath,
     privateKey,
-  }: Parameters<AbstractAddress<typeof AddressList.BNB>["importByPrivateKey"]>[0]): DerivedItem<
-    typeof AddressList.BNB
+  }: ImportByPrivateKeyParameters<typeof DerivationType.BNB>): DerivedCredential<
+    typeof DerivationType.BNB
   > {
-    const derivationPathWithoutAddress = removeDerivationPathAddress(derivationPath);
+    // const derivationPathWithoutAddress = removeDerivationPathAddress(derivationPath);
 
-    for (let i = 0; i < SEARCH_FROM_MNEMONIC_LIMIT; i++) {
-      const incrementedDerivationPath = appendAddressToDerivationPath(
-        derivationPathWithoutAddress,
-        i
-      );
+    // for (let i = 0; i < SEARCH_FROM_MNEMONIC_LIMIT; i++) {
+    //   const incrementedDerivationPath = appendAddressToDerivationPath(
+    //     derivationPathWithoutAddress,
+    //     i
+    //   );
 
-      const data = this.derive({ derivationPath: incrementedDerivationPath });
+    //   const data = this.derive({ derivationPath: incrementedDerivationPath });
 
-      if (data.privateKey === privateKey) return data;
-    }
+    //   if (data.privateKey === privateKey) return data;
+    // }
 
     const rawPrivateKey = toUint8Array(Buffer.from(privateKey, "hex"));
     const { publicKey } = this.getKeyPair(rawPrivateKey);
@@ -69,16 +68,14 @@ class BnbAddress extends Keys implements AbstractAddress<typeof AddressList.BNB>
       privateKey,
       publicKey,
       address,
-      mnemonic: EMPTY_MNEMONIC,
-      path: derivationPath,
     };
   }
 
-  private getAddress(privateKey: KeyPair["privateKey"]): string {
+  private getAddress(privateKey: DerivedKeyPair["privateKey"]): string {
     return getAddressFromPrivateKey(privateKey, HRP);
   }
 
-  private getKeyPair(rawPrivateKey?: Uint8Array): KeyPair {
+  private getKeyPair(rawPrivateKey?: Uint8Array): DerivedKeyPair {
     assert(rawPrivateKey, AddressError, ExceptionMessage.BNB_PRIVATE_KEY_GENERATION_FAILED);
     const keyPair: ECPairInterface = ecPair.fromPrivateKey(rawPrivateKey);
     const privateKey = toHexFromBytes(keyPair.privateKey);
