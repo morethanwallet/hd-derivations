680c2424621d6a3c0614c285694667a48444456b
paul <kyrogane1@gmail.com>
2025-01-20 11:30:52 +0000

    feat: Simplify typing

--- src/types/network/networkTypeUnion.type.ts
+++ src/types/network/networkTypeUnion.type.ts
@@ -1,3 +1,6 @@
-type NetworkTypeUnion = "btc" | "ada" | "avax";
+import { type ValueOf } from "ts-essentials";
+import { type NetworkTypeMap } from "./networkTypeMap.type.js";
+
+type NetworkTypeUnion = ValueOf<NetworkTypeMap>;
 
 export { type NetworkTypeUnion };
