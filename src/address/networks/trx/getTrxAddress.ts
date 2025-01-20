82d537320431e22e5c2434b45a5f421d63936991
paul <kyrogane1@gmail.com>
2025-01-20 12:14:30 +0000

    refactor: Pass trx address prefix as argument

--- src/address/networks/trx/getTrxAddress.ts
+++ src/address/networks/trx/getTrxAddress.ts
@@ -4,9 +4,8 @@ import { getEvmAddressBuffer } from "../helpers/index.js";
 import bs58check from "bs58check";
 import { toUint8Array } from "@/helpers/index.js";
 
-function getTrxAddress(publicKey: CommonKeyPair["publicKey"]): Address["address"] {
-  const addressPrefix = 0x41;
-  const addressPrefixBytes = toUint8Array(Buffer.from([addressPrefix]));
+function getTrxAddress(publicKey: CommonKeyPair["publicKey"], prefix: number): Address["address"] {
+  const addressPrefixBytes = toUint8Array(Buffer.from([prefix]));
   const addressBytes = toUint8Array(getEvmAddressBuffer(publicKey));
   const addressBufferWithPrefix = Buffer.concat([addressPrefixBytes, addressBytes]);
 
