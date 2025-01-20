3a0c6c2517576b34313342f9e2ba6a596e25ba10
paul <kyrogane1@gmail.com>
2025-01-20 11:50:22 +0000

    feat: Extend AbstractNetwork type

--- src/families/types/abstractNetwork.type.ts
+++ src/families/types/abstractNetwork.type.ts
@@ -2,11 +2,13 @@ import { type DerivationTypeUnion } from "@/types/index.js";
 import { type DeriveItemFromMnemonic } from "./deriveItemFromMnemonic.type.js";
 import { type GetCredentialFromPrivateKey } from "./getCredentialFromPrivateKey.type.js";
 import { type DeriveItemsBatchFromMnemonic } from "./deriveItemsBatchFromMnemonic.type.js";
+import { type CheckIfPrivateKeyBelongsToMnemonic } from "./checkIfPrivateKeyBelongsToMnemonic.type.js";
 
 type AbstractNetwork<TDerivationType extends DerivationTypeUnion> = {
-  deriveItemsBatchFromMnemonic: DeriveItemsBatchFromMnemonic<TDerivationType>;
   deriveItemFromMnemonic: DeriveItemFromMnemonic<TDerivationType>;
   getCredentialFromPrivateKey: GetCredentialFromPrivateKey<TDerivationType>;
+  deriveItemsBatchFromMnemonic: DeriveItemsBatchFromMnemonic<TDerivationType>;
+  checkIfPrivateKeyBelongsToMnemonic: CheckIfPrivateKeyBelongsToMnemonic<TDerivationType>;
 };
 
 export { type AbstractNetwork };
