3409645b435729cfe844f3fa49bfd5145b2bfac6
paul <kyrogane1@gmail.com>
2025-01-20 07:21:40 +0000

    refactor: Create getEvmAddressBuffer helper

--- src/address/networks/evm/getEvmAddress.ts
+++ src/address/networks/evm/getEvmAddress.ts
@@ -1,17 +1,10 @@
 import { type Address } from "@/address/types/index.js";
-import { checkAndRemoveEvmPublicKeyHexPrefix, toUint8Array } from "@/helpers/index.js";
 import { type CommonKeyPair } from "@/keyDerivation/types/index.js";
-import { addHexPrefix, importPublic, publicToAddress, toChecksumAddress } from "ethereumjs-util";
-
-function getPublicKeyBuffer(publicKey: Uint8Array): Buffer {
-  return Buffer.from(publicKey.buffer, publicKey.byteOffset, publicKey.byteLength);
-}
+import { addHexPrefix, toChecksumAddress } from "ethereumjs-util";
+import { getEvmAddressBuffer } from "../helpers/index.js";
 
 function getEvmAddress(publicKey: CommonKeyPair["publicKey"]): Address["address"] {
-  const unprefixedPublicKey = checkAndRemoveEvmPublicKeyHexPrefix(publicKey);
-  const publicKeyBuffer = getPublicKeyBuffer(toUint8Array(Buffer.from(unprefixedPublicKey, "hex")));
-  const evmPublicKey = importPublic(publicKeyBuffer);
-  const addressBuffer = publicToAddress(evmPublicKey);
+  const addressBuffer = getEvmAddressBuffer(publicKey);
   const hexAddress = addHexPrefix(addressBuffer.toString("hex"));
 
   return toChecksumAddress(hexAddress);
---
+++ src/address/networks/helpers/index.ts
@@ -0,0 +1,1 @@
+export { getEvmAddressBuffer } from "./getEvmAddressBuffer.helper.js";
