e4d4d1d03a8a7b0bd22c95fa7d430c34c1c84507
paul <kyrogane1@gmail.com>
2025-01-22 15:18:58 +0000

    fix: Make type more strict

--- src/families/helpers/findCustomConfig.helper.ts
+++ src/families/helpers/findCustomConfig.helper.ts
@@ -1,9 +1,21 @@
 import { type KeysConfig } from "@/keys/types/index.js";
-import { type DerivationTypeUnion } from "@/types/derivation/index.js";
+import {
+  type DerivationTypeMap,
+  type AdaDerivationTypeUnion,
+  type DerivationTypeUnion,
+} from "@/types/derivation/index.js";
+
+type KeysConfigRequiredDerivationTypeUnion = Exclude<
+  DerivationTypeUnion,
+  AdaDerivationTypeUnion | DerivationTypeMap["sol"] | DerivationTypeMap["tonBase"]
+>;
 
 function findCustomConfig(
-  derivationType: DerivationTypeUnion,
-  derivationConfigs: { derivationType: DerivationTypeUnion; keysConfig: KeysConfig }[]
+  derivationType: KeysConfigRequiredDerivationTypeUnion,
+  derivationConfigs: {
+    derivationType: KeysConfigRequiredDerivationTypeUnion;
+    keysConfig: KeysConfig;
+  }[]
 ): KeysConfig | void {
   return derivationConfigs.find((config) => config.derivationType === derivationType)?.keysConfig;
 }
