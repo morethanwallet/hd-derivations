60b468ccb5ab504a577e99010aa86ad65a5ab043
paul <kyrogane1@gmail.com>
2025-01-24 08:44:35 +0000

    refactor: Simplify SolanaKeyDerivation code

--- src/modules/keyDerivation/networks/solana/solanaKeyDerivation.ts
+++ src/modules/keyDerivation/networks/solana/solanaKeyDerivation.ts
@@ -4,19 +4,19 @@ import {
   type AbstractKeyDerivation,
   type DeriveFromMnemonicParameters,
 } from "@/modules/keyDerivation/libs/types/index.js";
-import { type PrivateKey, type CommonKeyPair } from "@/libs/types/index.js";
+import { type PrivateKey, type CommonKeyPair, type DerivationPath } from "@/libs/types/index.js";
 import edHd from "ed25519-hd-key";
 
 class SolanaKeyDerivation extends Ed25519Keys implements AbstractKeyDerivation<"sol"> {
   public deriveFromMnemonic({
     derivationPath,
   }: DeriveFromMnemonicParameters<"sol">): CommonKeyPair {
-    const { privateKey, publicKey } = this.getKeyPair(derivationPath);
+    const rawPrivateKey = edHd.derivePath(derivationPath, this.getHexSeed()).key;
+    const rawPublicKey = this.getRawPublicKey(rawPrivateKey);
+    const privateKey = base58.encode(Buffer.concat([rawPrivateKey, rawPublicKey]));
+    const publicKey = this.getBase58PublicKey(rawPublicKey);
 
-    return {
-      privateKey,
-      publicKey,
-    };
+    return { privateKey, publicKey };
   }
 
   public importByPrivateKey({ privateKey }: PrivateKey<"sol">): CommonKeyPair {
@@ -30,18 +30,6 @@ class SolanaKeyDerivation extends Ed25519Keys implements AbstractKeyDerivation<"
     );
 
     const rawPublicKey = this.getRawPublicKey(rawPrivateKey);
-    const publicKey = this.getBase58PublicKey(rawPublicKey);
-
-    return {
-      privateKey,
-      publicKey,
-    };
-  }
-
-  private getKeyPair(derivationPath: string): CommonKeyPair {
-    const rawPrivateKey = edHd.derivePath(derivationPath, this.getHexSeed()).key;
-    const rawPublicKey = this.getRawPublicKey(rawPrivateKey);
-    const privateKey = base58.encode(Buffer.concat([rawPrivateKey, rawPublicKey]));
     const publicKey = this.getBase58PublicKey(rawPublicKey);
 
     return { privateKey, publicKey };
