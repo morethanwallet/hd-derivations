f7717fa9999aabc700bbf749521fb210d5e36679
paul <kyrogane1@gmail.com>
2025-01-28 07:02:08 +0000

    feat: Add types for avax/trx configs

--- src/modules/network/libs/modules/config/avax-config.ts
+++ src/modules/network/libs/modules/config/avax-config.ts
@@ -1,8 +1,19 @@
 import { networks } from "bitcoinjs-lib";
+import type { CommonNetworkPurposeUnion } from "../../types/index.js";
+import type { PrefixConfig } from "@/libs/modules/keys/index.js";
 
 const COMMON_DERIVATION_PATH_PREFIX = "m/44'/9000'";
 
-const avaxConfig = {
+type AvaxConfig = {
+  [key in CommonNetworkPurposeUnion]: {
+    avax: {
+      derivationPathPrefix: string;
+      prefixConfig: PrefixConfig;
+    };
+  };
+};
+
+const avaxConfig: AvaxConfig = {
   mainnet: {
     avax: {
       derivationPathPrefix: COMMON_DERIVATION_PATH_PREFIX,
--- src/modules/network/libs/modules/config/trx-config.ts
+++ src/modules/network/libs/modules/config/trx-config.ts
@@ -1,6 +1,14 @@
+import type { PrefixConfig } from "@/libs/modules/keys/index.js";
 import { networks } from "bitcoinjs-lib";
 
-const trxConfig = {
+type TrxConfig = {
+  trxBase: {
+    derivationPathPrefix: string;
+    prefixConfig: PrefixConfig;
+  };
+};
+
+const trxConfig: TrxConfig = {
   trxBase: {
     derivationPathPrefix: "m/44'/195'",
     prefixConfig: { ...networks.bitcoin, pubKeyHash: 0x41 },
