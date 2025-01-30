325a67d8e26c8101b350e328784bc7b78865543a
paul <kyrogane1@gmail.com>
2025-01-30 17:14:58 +0000

    style: Update naming

--- src/modules/network/libs/modules/config/xrp-config.ts
+++ src/modules/network/libs/modules/config/xrp-config.ts
@@ -1,14 +1,14 @@
 import type { PrefixConfig } from "@/libs/modules/keys/index.js";
 import { getCommonPrefixConfig } from "./libs/helpers/index.js";
 
-type Config = {
+type XrpConfig = {
   prefixConfig: PrefixConfig;
   derivationPathPrefix: string;
 };
 
-const config: Config = {
+const xrpConfig: XrpConfig = {
   ...getCommonPrefixConfig("mainnet"),
   derivationPathPrefix: "m/44'/144'",
 };
 
-export { config };
+export { xrpConfig };
