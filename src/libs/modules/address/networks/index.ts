ff6d58e13156807e1a29960d60f0fed695d3082d
paul <kyrogane1@gmail.com>
2025-01-30 16:27:01 +0000

    chore: Remove re-export of not updated files

--- src/libs/modules/address/networks/index.ts
+++ src/libs/modules/address/networks/index.ts
@@ -6,14 +6,10 @@ export {
   getP2wshInP2shAddress,
 } from "./btc/index.js";
 export { getAvaxAddress } from "./avax/index.js";
-export { getBnbAddress } from "./bnb/index.js";
 export { getEnterpriseAddress, getRewardAddress, getBaseAddress } from "./ada/index.js";
-export { getEvmAddress } from "./evm/index.js";
 export { getXrpAddress } from "./xrp/index.js";
-export { getTransparentAddress } from "./zcash/index.js";
 export { getLegacyAddress } from "./common/index.js";
 export { getCashAddrAddress } from "./bch/index.js";
 export { getTrxAddress } from "./trx/get-trx-address.js";
 export { getTonAddress } from "./ton/index.js";
 export { getSuiAddress } from "./sui/get-sui-address.js";
-export { getDotAddress } from "./dot/get-dot-address.js";
