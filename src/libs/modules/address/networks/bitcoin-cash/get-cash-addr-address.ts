0f6aaa9e78e08d99ed840445bfa414da5523a437
paul <kyrogane1@gmail.com>
2025-01-30 08:32:34 +0000

    fix: Pass hex private key to getCashAddrAddress

--- src/libs/modules/address/networks/bitcoin-cash/get-cash-addr-address.ts
+++ src/libs/modules/address/networks/bitcoin-cash/get-cash-addr-address.ts
@@ -1,19 +1,25 @@
 import { ExceptionMessage } from "@/libs/modules/address/libs/enums/index.js";
 import { AddressError } from "@/libs/exceptions/index.js";
 import { type Address } from "@/libs/modules/address/libs/types/index.js";
-import { assert } from "@/libs/helpers/index.js";
+import { assert, toUint8Array } from "@/libs/helpers/index.js";
 import { type PrefixConfig } from "@/libs/modules/keys/index.js";
 import { toCashAddress } from "bchaddrjs";
 import { payments } from "bitcoinjs-lib";
+import type { CommonKeyPair } from "@/libs/types/index.js";
 
 const REGTEST_PREFIX = "bchreg";
 const HRP_DELIMITER = ":";
 const ADDRESS_INDEX = 1;
 
-function getCashAddrAddress(publicKey: Uint8Array, prefixConfig: PrefixConfig): Address["address"] {
+function getCashAddrAddress(
+  publicKey: CommonKeyPair["publicKey"],
+  prefixConfig: PrefixConfig,
+): Address["address"] {
+  const rawPublicKey = toUint8Array(Buffer.from(publicKey, "hex"));
+
   const { address } = payments.p2pkh({
     network: prefixConfig,
-    pubkey: publicKey,
+    pubkey: rawPublicKey,
   });
   assert(address, AddressError, ExceptionMessage.ADDRESS_GENERATION_FAILED);
   const formattedAddress = toCashAddress(address);
