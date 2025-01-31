9201d5ab2873a882b385b2b0379d1e029d0732d1
paul <kyrogane1@gmail.com>
2025-01-31 15:41:58 +0000

    style: Simplify type naming

--- src/modules/network/libs/types/abstract-network.type.ts
+++ src/modules/network/libs/types/abstract-network.type.ts
@@ -4,11 +4,11 @@ import { type GetCredentialFromPK } from "./get-credential-from-p-k.type.js";
 import { type DeriveItemsBatchFromMnemonic } from "./derive-items-batch-from-mnemonic.type.js";
 import { type DoesPKBelongToMnemonic } from "./does-p-k-belong-to-mnemonic.type.js";
 
-type AbstractNetwork<TDerivationType extends DerivationTypeUnion> = {
-  deriveItemFromMnemonic: DeriveItemFromMnemonic<TDerivationType>;
-  getCredentialFromPK: GetCredentialFromPK<TDerivationType>;
-  deriveItemsBatchFromMnemonic: DeriveItemsBatchFromMnemonic<TDerivationType>;
-  doesPKBelongToMnemonic: DoesPKBelongToMnemonic<TDerivationType>;
+type AbstractNetwork<T extends DerivationTypeUnion> = {
+  deriveItemFromMnemonic: DeriveItemFromMnemonic<T>;
+  getCredentialFromPK: GetCredentialFromPK<T>;
+  deriveItemsBatchFromMnemonic: DeriveItemsBatchFromMnemonic<T>;
+  doesPKBelongToMnemonic: DoesPKBelongToMnemonic<T>;
 };
 
 export { type AbstractNetwork };
