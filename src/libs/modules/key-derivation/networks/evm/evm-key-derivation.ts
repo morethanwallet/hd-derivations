a2d69e485198ca37d99edd0de81a065cd4122bf7
paul <kyrogane1@gmail.com>
2025-01-31 16:41:09 +0000

    fix: Correct not updated derivation types names

--- src/libs/modules/key-derivation/networks/evm/evm-key-derivation.ts
+++ src/libs/modules/key-derivation/networks/evm/evm-key-derivation.ts
@@ -9,14 +9,14 @@ import {
 } from "@/libs/modules/key-derivation/libs/types/index.js";
 import { type PrivateKey, type CommonKeyPair } from "@/libs/types/index.js";
 
-class EvmKeyDerivation extends Bip32Keys implements AbstractKeyDerivation<"evm"> {
+class EvmKeyDerivation extends Bip32Keys implements AbstractKeyDerivation<"evmBase"> {
   public constructor(prefixConfig: PrefixConfig, mnemonic: Mnemonic) {
     super(prefixConfig, mnemonic);
   }
 
   public deriveFromMnemonic({
     derivationPath,
-  }: DeriveFromMnemonicParameters<"evm">): CommonKeyPair {
+  }: DeriveFromMnemonicParameters<"evmBase">): CommonKeyPair {
     const node = this.rootKey.derivePath(derivationPath);
     const { privateKey, publicKey } = this.getKeyPair(node.privateKey);
 
@@ -26,7 +26,7 @@ class EvmKeyDerivation extends Bip32Keys implements AbstractKeyDerivation<"evm">
     };
   }
 
-  public importByPrivateKey({ privateKey }: PrivateKey<"evm">): CommonKeyPair {
+  public importByPrivateKey({ privateKey }: PrivateKey<"evmBase">): CommonKeyPair {
     const rawPrivateKey = toUint8Array(
       Buffer.from(checkAndRemoveEvmPublicKeyHexPrefix(privateKey), "hex"),
     );
