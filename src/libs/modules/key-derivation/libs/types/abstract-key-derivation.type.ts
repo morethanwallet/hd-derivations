343c86b72d3fd109565b2cfe70c372d6b7005bda
paul <kyrogane1@gmail.com>
2025-02-03 07:02:31 +0000

    style: Simplify type naming

--- src/libs/modules/key-derivation/libs/types/abstract-key-derivation.type.ts
+++ src/libs/modules/key-derivation/libs/types/abstract-key-derivation.type.ts
@@ -2,9 +2,9 @@ import { type ImportByPrivateKey } from "./import-by-private-key.type.js";
 import { type DerivationTypeUnion } from "@/libs/types/index.js";
 import { type DeriveFromMnemonic } from "./derive-from-mnemonic.type.js";
 
-type AbstractKeyDerivation<TDerivationType extends DerivationTypeUnion> = {
-  deriveFromMnemonic: DeriveFromMnemonic<TDerivationType>;
-  importByPrivateKey: ImportByPrivateKey<TDerivationType>;
+type AbstractKeyDerivation<T extends DerivationTypeUnion> = {
+  deriveFromMnemonic: DeriveFromMnemonic<T>;
+  importByPrivateKey: ImportByPrivateKey<T>;
 };
 
 export { type AbstractKeyDerivation };
