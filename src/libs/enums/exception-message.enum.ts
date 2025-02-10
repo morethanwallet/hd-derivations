d324316111f74be6185a9affd2234ef959e37a9b
paul <kyrogane1@gmail.com>
2025-02-10 06:39:16 +0000

    feat: Extend constants and enums

--- src/libs/enums/exception-message.enum.ts
+++ src/libs/enums/exception-message.enum.ts
@@ -1,6 +1,7 @@
 const ExceptionMessage = {
   INVALID_DERIVATION_PATH: "Invalid derivation path",
-  INVALID_SCHEME: "Invalid scheme",
+  INVALID_ALGORITHM: "Invalid algorithm",
+  PROVIDED_DATA_IS_INVALID: "Provided data is invalid",
 } as const;
 
 export { ExceptionMessage };
