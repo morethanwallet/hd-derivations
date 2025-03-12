49c69b244e3a992ade6ca37bd89e1c6bc8481707
paul <kyrogane1@gmail.com>
2025-03-12 10:45:32 +0000

    chore: Export derivation types

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,4 @@ export {
   type NetworkTypeUnion,
   type DerivedItem,
 } from "@/modules/network/index.js";
+export type { DerivationTypeUnion, GetDerivationTypeUnion } from "@/libs/types/index.js";
