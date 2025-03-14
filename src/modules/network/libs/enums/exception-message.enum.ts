ca11b9b92df5fbaed7a5bf7f54debb3f6a7871fd
paul <kyrogane1@gmail.com>
2025-03-14 09:33:28 +0000

    chore: Add error message for initialization error

--- src/modules/network/libs/enums/exception-message.enum.ts
+++ src/modules/network/libs/enums/exception-message.enum.ts
@@ -1,5 +1,6 @@
 const ExceptionMessage = {
   NETWORK_IS_NOT_SUPPORTED: "Network is not supported",
+  NETWORK_INITIALIZATION_FAILED: "Network initialization failed",
   ITEM_GENERATION_FAILED: "Item generation failed",
   CREDENTIAL_GENERATION_FAILED: "Credential generation failed",
 } as const;
