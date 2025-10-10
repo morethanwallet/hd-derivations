ae092a0517d3a6acbbbb34e0c0acaa9b3548961b
paul <kyrogane1@gmail.com>
2025-10-10 16:26:44 +0000

    feat(address): Implement Ada Exodus logic

---
+++ src/libs/modules/address/networks/ada/get-ada-exodus-address.ts
@@ -0,0 +1,11 @@
+import { BaseAddress } from "@emurgo/cardano-serialization-lib-nodejs";
+import { getCredential } from "./libs/helpers/index.js";
+
+function getAdaExodusAddress(publicKey: string, networkId: number): string {
+  const credential = getCredential(publicKey);
+  const address = BaseAddress.new(networkId, credential, credential);
+
+  return address.to_address().to_bech32();
+}
+
+export { getAdaExodusAddress };
